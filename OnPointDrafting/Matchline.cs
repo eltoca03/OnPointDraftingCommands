c7bb1c876abd89160dfe9b548c088f1cf306c461
eltoca03 <eltoca03@live.com>
2019-03-11 22:47:36 -0500

    Included MLN command, All command should be in this repository 03-11-209

---
+++ OnPointDrafting/Matchline.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+
+namespace OnPointDrafting
+{
+  public class Matchline
+  {
+	//get the current document and database
+	Document acDoc = Application.DocumentManager.MdiActiveDocument;
+	Database acCurDb = Application.DocumentManager.MdiActiveDocument.Database;
+
+	[CommandMethod("mln")]
+	public void mln()
+	{
+	  Editor ed = acDoc.Editor;
+
+	  //prompt select matchline
+	  PromptEntityOptions entOpt = new PromptEntityOptions("Select Matchline");
+	  entOpt.SetRejectMessage("Line Selected is not Matchline\n");
+	  entOpt.AddAllowedClass(typeof(Line), false);
+
+	  //check entity results status
+	  PromptEntityResult entRst = ed.GetEntity(entOpt);
+	  if (entRst.Status != PromptStatus.OK)
+		return;
+
+	  //begin transaction
+	  Transaction trans = acCurDb.TransactionManager.StartTransaction();
+
+	  using (trans)
+	  {
+		//open blockTable for read
+		BlockTable blockTable;
+		blockTable = trans.GetObject(acCurDb.BlockTableId, OpenMode.ForRead) as BlockTable;
+
+		BlockTableRecord blockTableRecord;
+		blockTableRecord = trans.GetObject(blockTable[BlockTableRecord.ModelSpace],
+										   OpenMode.ForWrite) as BlockTableRecord;
+
+		//i think i need to cast the entity to a polyline so i can get the point
+		Line matchLine = trans.GetObject(entRst.ObjectId, OpenMode.ForWrite) as Line;
+
+		//get the midpoint of line
+		Point3d pts = getMidPoint(matchLine);
+
+		//create text
+		DBText mText = new DBText();
+
+		//System.Diagnostics.Debugger.Launch();
+
+		mText.Justify = AttachmentPoint.MiddleCenter;
+		mText.LayerId = matchLine.LayerId;
+		mText.Height = 5;
+
+		//prompt to select side 
+		PromptPointOptions ptOpt = new PromptPointOptions("Select Side ");
+
+		PromptPointResult ptResult = ed.GetPoint(ptOpt);
+
+		if (!sideSelected(ptResult.Value, matchLine))
+		{
+		  //mText.Justify = AttachmentPoint.TopCenter;
+		  Line x = matchLine.GetOffsetCurves(-3.75)[0] as Line;
+		  mText.AlignmentPoint = getMidPoint(x);
+		}
+		else
+		{
+		  Line x = matchLine.GetOffsetCurves(3.75)[0] as Line;
+		  mText.AlignmentPoint = getMidPoint(x);
+		}
+
+		//prompt user for sheet number
+		PromptStringOptions strOpt = new PromptStringOptions("Enter Page #")
+		{
+		  AllowSpaces = false
+		};
+
+		TextStyleTable textStyleTable = trans.GetObject(acCurDb.TextStyleTableId, OpenMode.ForRead) as TextStyleTable;
+
+		if (textStyleTable.Has("B"))
+		{
+		  mText.TextStyleId = trans.GetObject(textStyleTable["B"], OpenMode.ForRead).ObjectId;
+		}
+
+		PromptResult sheetNum = ed.GetString(strOpt);
+		mText.TextString = "MATCH LINE - SEE SHEET " + sheetNum.StringResult;
+
+
+		if (matchLine.Angle > (Math.PI / 2) && matchLine.Angle < ((Math.PI * 3) / 2))
+		{
+		  mText.Rotation = matchLine.Angle + Math.PI;
+		}
+		else
+		{
+		  mText.Rotation = matchLine.Angle;
+		}
+
+
+
+		//append to block table
+		blockTableRecord.AppendEntity(mText);
+		trans.AddNewlyCreatedDBObject(mText, true);
+		trans.Commit();
+
+	  }
+	}
+
+	public static PlanarEntity getPlane()
+	{
+	  return PlanarEntity.Create(IntPtr.Zero, true);
+	}
+
+	//calculate the midpoint 
+	public Point3d getMidPoint(Line a)
+	{
+
+
+	  //DBObjectCollection objcoll = a;
+	  //Line tempLine = new Line();
+
+	  //foreach (Entity ent in objcoll)
+	  //{
+	  //    tempLine = ((Line)ent);
+	  //}
+
+	  Point3d point3d = new Point3d();
+	  Vector3d vector = new Vector3d();
+	  vector = a.StartPoint.GetVectorTo(a.EndPoint);
+	  point3d = a.StartPoint + (vector / 2);
+
+	  return point3d;
+	}
+
+	//create vector with point selected and midpoint of line 
+	//crete vector with midpoint and endpoint of line
+	public bool sideSelected(Point3d sideSelectedPt, Line line)
+	{
+	  Line lineTemp = new Line(line.StartPoint, sideSelectedPt);
+
+	  if (line.Angle == 0)
+	  {
+		if (lineTemp.Angle < Math.PI)
+		  return true;
+		else { return false; }
+	  }
+	  else if (lineTemp.Angle >= line.Angle)
+	  {
+		return true;
+	  }
+	  else
+	  {
+		return false;
+	  }
+	}
+  }
+}
---
+++ OnPointDrafting2013/Stations.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+
+namespace OnPointDrafting
+{
+  public class Stations
+  {
+	Document doc = Application.DocumentManager.MdiActiveDocument;
+	Database database = Application.DocumentManager.MdiActiveDocument.Database;
+
+	[CommandMethod("sta")]
+	public void Sta()
+	{
+	  Editor ed;
+	  ed = doc.Editor;
+
+	  PromptEntityOptions peo = new PromptEntityOptions("\nSelect running line: ");
+	  peo.SetRejectMessage("\nNot a polyline");
+	  peo.AddAllowedClass(typeof(Polyline), false);
+	  PromptEntityResult per = ed.GetEntity(peo);
+
+	  if (per.Status != PromptStatus.OK) return;
+
+	  PromptStringOptions pso = new PromptStringOptions("\nSelect text side Bottom <Top>: ");
+	  PromptResult pr = ed.GetString(pso);
+
+	  if (pr.Status != PromptStatus.OK) return;
+
+	  bool flip = false;
+
+	  while (true)
+	  {
+		if (pr.StringResult.Length > 0)
+		{
+		  if (pr.StringResult.ToUpper().Contains("T") || pr.StringResult.ToUpper().Contains("TOP"))
+		  {
+			break;
+		  }
+		  else if (pr.StringResult.ToUpper().Contains("B") || pr.StringResult.ToUpper().Contains("BOTTOM"))
+		  {
+			flip = true;
+			break;
+		  }
+		  else
+		  {
+			pr = ed.GetString(pso);
+			if (pr.Status != PromptStatus.OK) return;
+		  }
+		}
+		break; 
+	  }
+
+
+	  /****************************************/
+	  int interval = 100;
+	  pso = new PromptStringOptions("\nSelect interval <100>: ");
+	  pr = ed.GetString(pso);
+
+	  if (pr.Status != PromptStatus.OK) return;
+
+	  while (true)
+	  {
+		if (pr.StringResult.Length > 0)
+		{
+		  if (int.TryParse(pr.StringResult, out int ignoreme))
+		  {
+			interval = int.Parse(pr.StringResult);
+			break;
+		  }
+		  else
+		  {
+			pso = new PromptStringOptions("\nPlease enter an interger: ");
+			pr = ed.GetString(pso);
+			if (pr.Status != PromptStatus.OK) return;
+		  }
+		}
+		else break;
+	  }
+	  /****************************************/
+	  //busca este commentario online eltoca03..drafting
+	  Transaction trans = database.TransactionManager.StartTransaction();
+
+	  using (trans)
+	  {
+		var curSpace = (BlockTableRecord)trans.GetObject(database.CurrentSpaceId, OpenMode.ForWrite);
+		Polyline polyline = (Polyline)trans.GetObject(per.ObjectId, OpenMode.ForRead);
+
+		Double len = polyline.Length;
+		int count = (int)len / interval;
+
+		for (int i = 0; i <= count; i++)
+		{
+		  Point3d p1 = polyline.GetPointAtDist(i * interval);
+		  Vector3d ang = polyline.GetFirstDerivative(polyline.GetParameterAtPoint(p1));
+		  //scale the vector by 1.5
+		  ang = ang.GetNormal() * 1.5;
+		  //rotate the vector
+		  ang = ang.TransformBy(Matrix3d.Rotation(Math.PI / 2, polyline.Normal, Point3d.Origin));
+		  // create a line by substracting and adding the vector to the point (displacing the point
+		  Line line = new Line(p1 - ang, p1 + ang);
+		  line.Layer = "Text-2";
+		  //create mtext place end of new line 
+		  MText dBText = new MText();
+		  //format numbers for context
+		  dBText.Contents = FormatStation(i* interval);
+		  
+		  dBText.BackgroundFill = true;
+		  dBText.BackgroundScaleFactor = 1.25;
+		  if (flip)
+		  {
+			if (line.Angle > Math.PI)
+			{
+			  dBText.Attachment = AttachmentPoint.BottomCenter;
+			}
+			else
+			{
+			  dBText.Attachment = AttachmentPoint.TopCenter;
+			}
+			
+			dBText.Location = p1 - (ang.GetNormal() * 2);
+		  }
+		  else
+		  {
+			if (line.Angle > Math.PI)
+			{
+			  dBText.Attachment = AttachmentPoint.TopCenter;
+			}
+			else
+			{
+			  dBText.Attachment = AttachmentPoint.BottomCenter;
+			}
+			
+			dBText.Location = p1 + (ang.GetNormal() * 2);
+		  }
+		  
+		  dBText.Layer = "Text-2";
+		  dBText.Height = 2.2;
+
+		  if (line.Angle > (Math.PI / 2) && (line.Angle <= Math.PI))
+		  {
+			dBText.Rotation = line.Angle - (Math.PI / 2);
+		  }
+		  else if (line.Angle > Math.PI)
+		  {
+			dBText.Rotation = line.Angle + (Math.PI / 2);
+		  }
+		  else
+		  {
+			dBText.Rotation = (2 * Math.PI) - Math.Abs(line.Angle - (Math.PI / 2));
+		  }
+
+		  curSpace.AppendEntity(dBText);
+		  trans.AddNewlyCreatedDBObject(dBText, true);
+
+		  curSpace.AppendEntity(line);
+		  trans.AddNewlyCreatedDBObject(line, true);
+		}
+		trans.Commit();
+	  }
+	  
+	}
+
+	private string FormatStation(int v)
+	{
+	  string formattedStation = "";
+
+	  if (v.ToString().Length == 1)
+	  {
+		formattedStation = "0+0" + v.ToString();
+	  }
+	  else if (v.ToString().Length == 2)
+	  {
+		formattedStation = "0+" + v.ToString();
+	  }
+	  else
+	  {
+		char[] array = v.ToString().ToCharArray();
+
+		for (int i = 0; i < array.Length; i++)
+		{
+		  if (array.Length - i == 2)
+		  {
+			formattedStation += "+";
+		  }
+
+		  formattedStation += array[i];
+		}
+
+	  }
+	  return formattedStation;
+	}
+
+	[CommandMethod("ft")]
+	public void Flip()
+	{
+	  Editor ed = doc.Editor;
+	  
+	  PromptEntityOptions peo = new PromptEntityOptions("\nSelect station to flip: ");
+	  peo.SetRejectMessage("\nStation must be MTEXT");
+	  peo.AddAllowedClass(typeof(MText), false);
+	  PromptEntityResult per = ed.GetEntity(peo);
+	  if (per.Status != PromptStatus.OK) return;
+
+	  //PromptStringOptions pso = new PromptStringOptions("Up <Down>: ");
+	  //PromptResult pr = ed.GetString(pso);
+
+	  //if (pr.Status != PromptStatus.OK) return;
+
+	  Transaction trans = database.TransactionManager.StartTransaction();
+
+	  using (trans)
+	  {
+		MText station = (MText)trans.GetObject(per.ObjectId, OpenMode.ForWrite);
+
+		//if (pr.StringResult.ToUpper().Contains("U"))
+		//{
+		//  station.Location = GetPoint(station.Location.X, station.Location.Y, station.Rotation + (Math.PI * -1.5));
+		//}
+		//else
+		//{
+		//  station.Location = GetPoint(station.Location.X, station.Location.Y, station.Rotation + (Math.PI * 1.5));
+		//}
+
+		if (station.Attachment == AttachmentPoint.TopCenter)
+		{
+		  station.Location = GetPoint(station.Location.X, station.Location.Y, station.Rotation + (Math.PI * -1.5));
+		  station.Attachment = AttachmentPoint.BottomCenter;
+		}
+		else
+		{
+		  station.Location = GetPoint(station.Location.X, station.Location.Y, station.Rotation + (Math.PI * 1.5));
+		  station.Attachment = AttachmentPoint.TopCenter;
+		}
+		
+		trans.Commit();
+	  }
+	  
+	}
+
+	public Point3d GetPoint(double x, double y, double ang)
+	{
+	  Point3d pt = new Point3d(new double[] { 4.2 * Math.Cos(ang) + x, 4.2 * Math.Sin(ang) + y, 0 });
+
+	  return pt;
+	}
+	
+  }
+}
