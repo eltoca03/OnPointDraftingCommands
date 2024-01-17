22d6bc204a47e4c22802ff013fe4194e31ab072f
eltoca03 <eltoca03@live.com>
2024-01-17 00:31:57 -0600

    Update assembly name and versioning

--- OnPointDrafting/Properties/AssemblyInfo.cs
+++ OnPointDrafting/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("OnPointDrafting")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("dsalcad")]
 [assembly: AssemblyProduct("OnPointDrafting")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.2.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
